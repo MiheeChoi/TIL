917b737d895170e56c9d1a12319b5c70be136f73
Replit user <>
2023-02-17 13:26:36 +0000

    데이터형 실습

--- Python/nomad_coders_challenge/tax_calculator/main.py
+++ Python/nomad_coders_challenge/tax_calculator/main.py
@@ -1,35 +1,5 @@
-# Write your code here:
-def get_yearly_revenue(monthly_revenue):
-  return 12 * monthly_revenue
+#tuple
+fruit = ('apple', 'banana',["strawberry","blueberry"])
+print(fruit)
+# 튜플에도 리스트 형의 데이터 추가 가능
 
-
-def get_yearly_expenses(monthly_expenses):
-  return 12 * monthly_expenses
-
-
-def get_tax_amount(profit):
-  if (profit > 100000):
-    tax_amount = profit * 0.25
-  else:
-    tax_amount = profit * 0.15
-  return tax_amount
-
-
-def apply_tax_credits(tax_amount, tax_credits):
-  return tax_amount * tax_credits
-
-
-# Don't touch anthing below this line 🙅🏻‍♂️🙅🏻‍♀️
-
-monthly_revenue = 5500000
-monthly_expenses = 2700000
-tax_credits = 0.01
-
-profit = get_yearly_revenue(monthly_revenue) - get_yearly_expenses(
-  monthly_expenses)
-
-tax_amount = get_tax_amount(profit)
-
-final_tax_amount = tax_amount - apply_tax_credits(tax_amount, tax_credits)
-
-print(f"Your tax bill is: ${final_tax_amount}")
