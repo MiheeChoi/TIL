c63624e2d1ade618186ab09c839ad07f0555ff4d
MiheeChoi <chemh9738@gmail.com>
2023-02-18 21:11:00 +0900

    forloop challenge

--- Python/nomad_coders_challenge/web_checker/web_checker.py
+++ Python/nomad_coders_challenge/web_checker/web_checker.py
@@ -1,6 +1,15 @@
+from requests import get
+
 websites = {
-    "google.com"
-    "naver.com"
-    "twitter.com"
-    "facebook.com"
+    "google.com",
+    "https://naver.com",
+    "twitter.com",
+    "facebook.com",
+    "https://airbnb.com"
 }
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    print(response)
