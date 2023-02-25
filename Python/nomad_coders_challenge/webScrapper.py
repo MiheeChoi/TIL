b5c22bc6e22c8063c4d2a9458a74efdba2edd960
MiheeChoi <chemh9738@gmail.com>
2023-02-25 17:28:26 +0900

    weworkremotely web scrapper ver1

--- Python/nomad_coders_challenge/webScrapper.py
+++ Python/nomad_coders_challenge/webScrapper.py
@@ -1,13 +1,33 @@
+from requests import get
 from bs4 import BeautifulSoup
-import requests
 
-def extract_jobs(term):
-  url = f"https://remoteok.com/remote-{term}-jobs"
-  request = requests.get(url, headers={"User-Agent": "Kimchi"})
-  if request.status_code == 200:
-    soup = BeautifulSoup(request.text, "html.parser")
-    # write your ✨magical✨ code here
-  else:
-    print("Can't get jobs.")
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
 
-extract_jobs("rust")
+search_term = "java"
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+  print("can't request website")
+else:
+  results = []
+  soup = BeautifulSoup(response.text, "html.parser")
+  jobs = (soup.find_all('section', class_='jobs'))
+  for job_section in jobs:
+    post_list = job_section.find_all('li')
+    post_list.pop(-1)
+    for job_post in post_list:
+      anchors = job_post.find_all('a')
+      anchor = anchors[1]
+      link = anchor['href']
+      company, kind, region = anchor.find_all('span', class_='company')
+      title = anchor.find('span', class_='title')
+      job_data = {
+        'link': f"https://weworkremotely.com/{link}",
+        'company': company.string,
+        'kind': kind.string,
+        'region': region.string,
+        'position': title.string
+      }
+      results.append(job_data)
+for result in results:
+  print(result)
+  print('/////')
